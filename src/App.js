b9f682be651406691ff2ab67e19fcdb1318fddfa
pikselan <faif@pikselan.com>
2020-06-29 10:04:11 +0700

    set tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="mx-auto max-w-sm rounded overflow-hidden shadow-lg">
+      <img className="w-full" src={require("./profile.jpg")} alt="Display" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">Faiff</div>
+        <p className="text-gray-700 text-base">
+          When i’m not coding i switch to netflix with biscuits and cold tea as
+          my companion. <span></span>😜
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="px-6 py-4">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Software Engineer
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+          #Gamer
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2 ml-20">
+          #Sleeper
+        </span>
+      </div>
     </div>
   );
 }
